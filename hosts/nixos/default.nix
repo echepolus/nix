20012e3717dbf0d83f60c1231d8e169806cee014
echepolus <a.kotominn@gmail.com>
2025-10-27 01:58:37 +0300

    tt

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -250,12 +250,12 @@ in
         "docker"
       ];
       shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = keys;
+      # openssh.authorizedKeys.keys = keys;
     };
 
-    root = {
-      openssh.authorizedKeys.keys = keys;
-    };
+    # root = {
+    #   openssh.authorizedKeys.keys = keys;
+    # };
   };
 
   # Don't require password for users in `wheel` group for these commands
